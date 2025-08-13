034750927ca4a6235508e7127e16153ea1537410
NamEon <143885031+skadjs@users.noreply.github.com>
2025-08-14 02:23:56 +0900

    객체 주입

--- src/main/java/com/rookies3/myspringbootlab/MyPropRunner.java
+++ src/main/java/com/rookies3/myspringbootlab/MyPropRunner.java
@@ -1,5 +1,6 @@
 package com.rookies3.myspringbootlab;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,17 +9,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyPropRunner implements ApplicationRunner {
 
-    @Value("${myprop.username}")
-    private String username;
+//    @Value("${myprop.username}")
+//    private String username;
 
-    @Value("${myprop.port}")
-    private int port;
+//    @Value("${myprop.port}")
+//    private int port;
+
+    @Autowired
+    private MyPropProperties props;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("========= @Value 어노테이션으로 값 읽기 =========");
-        System.out.println("myprop.username = " + username);
-        System.out.println("myprop.port = " + port);
+//      System.out.println("myprop.username = " + username);
+        System.out.println("myprop.username = " + props.getUsername());
+//      System.out.println("myprop.port = " + port);
+        System.out.println("myprop.port = " + props.getPort());
         System.out.println("==============================================");
     }
 }
