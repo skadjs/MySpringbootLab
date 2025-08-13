928cd491c54dfd8b067fc5c0dbc86db244f80042
NamEon <143885031+skadjs@users.noreply.github.com>
2025-08-14 02:33:34 +0900

    MyPropRunner 클래스 수정

--- src/main/java/com/rookies3/myspringbootlab/MyPropRunner.java
+++ src/main/java/com/rookies3/myspringbootlab/MyPropRunner.java
@@ -1,5 +1,7 @@
 package com.rookies3.myspringbootlab;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -9,22 +11,31 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyPropRunner implements ApplicationRunner {
 
-//    @Value("${myprop.username}")
-//    private String username;
+    private static final Logger logger = LoggerFactory.getLogger(MyPropRunner.class);
+//  @Value("${myprop.username}")
+//  private String username;
 
-//    @Value("${myprop.port}")
-//    private int port;
+//  @Value("${myprop.port}")
+//  private int port;
 
     @Autowired
     private MyPropProperties props;
 
+    @Autowired(required = false)
+    private MyEnvironment myEnv;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("========= @Value 어노테이션으로 값 읽기 =========");
-//      System.out.println("myprop.username = " + username);
-        System.out.println("myprop.username = " + props.getUsername());
-//      System.out.println("myprop.port = " + port);
-        System.out.println("myprop.port = " + props.getPort());
-        System.out.println("==============================================");
+        logger.info("========= 로거로 값 출력하기 =========");
+        logger.debug("myprop.username (DEBUG) = {}", props.getUsername());
+        logger.info("myprop.username (INFO) = {}", props.getUsername());
+        logger.info("myprop.port = {}", props.getPort());
+        logger.info("=====================================");
+
+        if (myEnv != null) {
+            logger.info(">> 현재 활성화된 프로파일: {}", myEnv.getMode());
+        } else {
+            logger.info(">> 활성화된 프로파일이 없습니다.");
+        }
     }
 }
