64d35ea7b541a6012d548fbeb8e0f982f9fbc115
NamEon <143885031+skadjs@users.noreply.github.com>
2025-08-14 02:16:41 +0900

    first commit

---
+++ src/main/java/com/rookies3/myspringbootlab/MyPropRunner.java
@@ -0,0 +1,24 @@
+package com.rookies3.myspringbootlab;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyPropRunner implements ApplicationRunner {
+
+    @Value("${myprop.username}")
+    private String username;
+
+    @Value("${myprop.port}")
+    private int port;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("========= @Value 어노테이션으로 값 읽기 =========");
+        System.out.println("myprop.username = " + username);
+        System.out.println("myprop.port = " + port);
+        System.out.println("==============================================");
+    }
+}
---
+++ src/main/java/com/rookies3/myspringbootlab/MySpringbootLabApplication.java
@@ -0,0 +1,12 @@
+package com.rookies3.myspringbootlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootLabApplication.class, args);
+	}
+}
