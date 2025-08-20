d6fcf4154396267e2f01965671629a3cad1cfde0
NamEon <143885031+skadjs@users.noreply.github.com>
2025-08-20 11:49:02 +0900

    refactor: 클래스 위치 변경 -BookRepositoryTest 클래스의 위치가 잘못되어 올바른 디렉토리로 옮김.

--- src/test/java/com/example/book/BookRepositoryTest.java
+++ src/test/java/com/example/book/BookRepositoryTest.java
@@ -1,8 +1,10 @@
-package com.example.book.repository;
+package com.example.book;
 
 import com.example.book.entity.Book;
+import com.example.book.repository.BookRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
